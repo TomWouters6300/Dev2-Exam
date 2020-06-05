2e0cc5bd7102750a241562ec07ea2ae6192371e4
Robin Van Roy <46923671+thatdudemanguy@users.noreply.github.com>
2020-06-05 13:05:43 +0200

    Initial commit

---
+++ oefening-1-objects-and-arrays/index.js
@@ -0,0 +1,66 @@
+class Phones {
+  constructor() {
+    this.data = [
+      {
+        name: 'IPHONE 11',
+        brand: 'Apple',
+        price: 809,
+        image: 'https://cdn.mos.cms.futurecdn.net/rJFKqhNF7ZEdPBDg6LdQ4K-768-80.png.webp'
+      },
+      {
+        name: 'ONEPLUS 8 PRO',
+        brand: 'Oneplus',
+        price: 899,
+        image: 'https://cdn.mos.cms.futurecdn.net/GshBbgyzfXcuyzB2ySDV7W-768-80.jpg.webp'
+      },
+      {
+        name: 'IPHONE SE (2020)',
+        brand: 'Apple',
+        price: 499,
+        image: 'https://cdn.mos.cms.futurecdn.net/Pv5UY9QGTWkr2CPA6vkN9i-768-80.png.webp'
+      },
+      {
+        name: 'SAMSUNG GALAXY S20 ULTRA',
+        brand: 'Samsung',
+        price: 959,
+        image: 'https://cdn.mos.cms.futurecdn.net/zPSGkRzoJBtu4NEEaDQCch-768-80.jpg.webp'
+      },
+      {
+        name: 'SAMSUNG GALAXY NOTE 10',
+        brand: 'Samsung',
+        price: 622,
+        image: 'https://cdn.mos.cms.futurecdn.net/ztJWkHq52UfKqgn8eVgZQZ-768-80.jpg.webp'
+      },
+      {
+        name: 'IPHONE 11 PRO',
+        brand: 'Apple',
+        price: 1000,
+        image: 'https://cdn.mos.cms.futurecdn.net/no6isXQdp64V3bieFpsLeh-768-80.png.webp'
+      },
+      {
+        name: 'GOOGLE PIXEL 4',
+        brand: 'Google',
+        price: 555,
+        image: 'https://cdn.mos.cms.futurecdn.net/Wv2kBhTCXStaAFRESERP3P-768-80.jpg.webp'
+      },
+      {
+        name: 'SAMSUNG GALAXY S10 PLUS',
+        brand: 'Samsung',
+        price: 629,
+        image: 'https://cdn.mos.cms.futurecdn.net/Vxj8VvpPJrRHSXLV86Dvfb-768-80.jpg.webp'
+      },
+      {
+        name: 'SONY XPERIA 1',
+        brand: 'Sony',
+        price: 949,
+        image: 'https://cdn.mos.cms.futurecdn.net/thBhakZeuB9GhTN2oENBYU-768-80.jpg.webp'
+      },
+      {
+        name: 'GOOGLE PIXEL 3A',
+        brand: 'Google',
+        price: 314,
+        image: 'https://cdn.mos.cms.futurecdn.net/3xXeBSvWNmAJdYZtfpzeuF-768-80.jpg.webp'
+      },
+    ];
+  }
+}
